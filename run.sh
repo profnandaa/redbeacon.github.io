88bb133369995c7b415cfc674527d4533dbdf348
Billy McCarthy <billy@redbeacon.com>
2013-11-07 19:25:20 +0000

    run the server on a special port for each user

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright 2013 Red Beacon, Inc. - All Rights Reserved
+#
+# Author: Billy McCarthy
+
+uid=`id -u $USER`
+port=`expr 40000 + $uid`
+
+pkill -U $uid -f 'manage.py runserver'
+/opt/bin/jekyll serve --watch -P $port
